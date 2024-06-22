eb1cd40b1fd8144e7f5dbb4c4ea34b6eb8e715fa
Ifeoluwa <ifel1939@gmail.com>
2024-06-22 16:05:25 +0100

    site controller

--- microservices/site/controller/SiteController.js
+++ microservices/site/controller/SiteController.js
@@ -1,4 +1,4 @@
-const Board = require("../model/Board");
+const Site = require("../model/Site");
 const { StatusCodes } = require("http-status-codes");
 const {
   CustomError,
@@ -32,3 +32,131 @@ const {
 //get all sites assigned to fse
 //get site details
 //update site details
+
+const createSite = async (req, res) => {
+  const {
+    siteName,
+    latitude,
+    longitude,
+    address,
+    topography,
+    type,
+    location,
+    region,
+    controller,
+    components,
+  } = req.body;
+
+  let site = new Site({
+    siteName,
+    latitude,
+    longitude,
+    address,
+    topography,
+    type,
+    location,
+    region,
+    controller,
+    components,
+  });
+
+  site = await site.save();
+  res.status(StatusCodes.CREATED).json({ site: site });
+};
+
+const getSingleSite = async (req, res) => {
+  const siteId = req.params.id;
+
+  if (!siteId) {
+    throw new BadrequestError(`Please provide site id to get board details`);
+  }
+
+  const site = await Site.findOne({ _id: siteId });
+
+  if (!site) {
+    throw new NotFoundError(
+      `board with id ${siteId} not found or does not exist`
+    );
+  }
+
+  res.status(StatusCodes.OK).json({ site: site });
+};
+
+const getAllSite = async (req, res) => {
+  const sites = await Site.find({});
+  res.status(StatusCodes.OK).json({ sites: sites });
+};
+
+const fixedSite = async (req, res) => {
+  const siteId = req.params.id;
+  const { controller, components } = req.body;
+
+  if (!controller || components) {
+    throw new BadrequestError(
+      `Please provide board id  and update status to repair board`
+    );
+  }
+
+  const site = await Site.findOne({ _id: siteId });
+
+  if (!site) {
+    throw new NotFoundError(
+      `site with id ${siteId} not found or does not exist`
+    );
+  }
+
+  site.controller = controller;
+  site.components = components;
+  await site.save();
+
+  res.status(StatusCodes.OK).json({ site: site, msg: "sla status updated" });
+};
+
+const updateSite = async (req, res) => {
+  const id = req.params.id;
+  const {
+    siteName,
+    latitude,
+    longitude,
+    address,
+    topography,
+    type,
+    location,
+    region,
+    controller,
+    components,
+  } = req.body;
+
+  const site = await Site.findOne({ _id: site });
+
+  if (!site) {
+    throw new NotFoundError(
+      `board with id ${siteId} not found or does not exist`
+    );
+  }
+
+  site.siteName = siteName;
+  site.latitude = latitude;
+  site.longitude = longitude;
+
+  site.address = address;
+  site.topography = topography;
+  site.type = type;
+  site.location = location;
+  site.region = region;
+  site.controller = controller;
+  site.components = components;
+  await site.save();
+
+  res
+    .status(StatusCodes.OK)
+    .json({ site: site, msg: "board details updated successfully" });
+};
+
+module.exports = {
+  createSite,
+  getSingleSite,
+  getAllSite,
+  fixedSite,
+  updateSite,
+};
