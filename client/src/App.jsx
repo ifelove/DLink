edc8c3d9db064f08c4adb49e713ea24bfc5029fb
Ifeoluwa <ifel1939@gmail.com>
2024-06-27 23:39:01 +0100

    colapsing and expanding sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
+import React from "react";
 import FixedSideBar from "./component/FixedSideBar";
 import SideBar from "./component/SideBar";
 
 function App() {
+  const [isCollapsed, setIsCollapsed] = React.useState(true);
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-  
-    <section>
-      <FixedSideBar />
-      <SideBar />
+    <section className="flex">
+      <article>
+        <FixedSideBar />
+        <SideBar isCollapsed={isCollapsed} toggleSidebar={toggleSidebar} />
+      </article>
+      <div className={`main-content ${isCollapsed ? "expanded" : ""}`}>
+        <h1>Welcome to the Collapsible Sidebar Example</h1>
+        <p>Here is some main content.</p>
+      </div>
     </section>
-   
   );
 }
 
--- client/src/component/SideBar.jsx
+++ client/src/component/SideBar.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from "react";
 import { IoIosArrowDroprightCircle } from "react-icons/io";
 import { IoIosArrowDropleftCircle } from "react-icons/io";
 
-const SideBar = () => {
-const [isCollapsed, setIsCollapsed] = React.useState(true);
-const toggleSidebar = () => {}
-
+const SideBar = ({ isCollapsed, toggleSidebar }) => {
+ 
 
   return (
     <div className={`sidebar ${isCollapsed ? "collapsed" : ""}`}>
-        
-      <button onClick={toggleSidebar} className="toggle-btn">
-        {isCollapsed ? "Expand" : "Collapse"}
-      </button>
-      {!isCollapsed && (
-        <div>
-         Hello
-        </div>
+      {isCollapsed ? (
+        <IoIosArrowDroprightCircle
+          className="expand-icon"
+          onClick={toggleSidebar}
+        />
+      ) : (
+        <IoIosArrowDropleftCircle
+          className="expand-icon"
+          onClick={toggleSidebar}
+        />
       )}
+      {!isCollapsed && <div>Hello</div>}
     </div>
   );
-}
+};
 
-export default SideBar
+export default SideBar;
