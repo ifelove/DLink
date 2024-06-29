bbeb204466781784c7feaf9b921a32799053f54e
Ifeoluwa <ifel1939@gmail.com>
2024-06-29 08:48:30 +0100

    navbar title display position fixed

--- client/src/component/NavBar.jsx
+++ client/src/component/NavBar.jsx
@@ -11,41 +11,42 @@ import { Badge, Button } from "rsuite";
 
 const NavBar = ({ theme, toggleTheme }) => {
   return (
-    <div className="navbar bg-base-100 ">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Tools</a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li className="mr-5" title="Theme">
-            <MdDarkMode className="i alert " onClick={toggleTheme} />
-            <div style={{ height: 0 }} className="nav-tooltip">
-              <Tooltip visible>Theme</Tooltip>
-            </div>
-          </li>
+    <>
+      <div className="navbar bg-base-100 ">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">Tools</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li className="mr-5" title="Theme">
+              <MdDarkMode className="i alert " onClick={toggleTheme} />
+            </li>
 
-          <li className=" " title="Noticification">
-            <Badge content={0}>
-              <MdOutlineNotifications className="i alert rounded-md" />
-            </Badge>
-            <div style={{ height: 0 }}>
-              <Tooltip visible>Noticification</Tooltip>
+            <li className=" " title="Noticification">
+              <Badge content={0}>
+                <MdOutlineNotifications className="i alert rounded-md" />
+              </Badge>
+            </li>
+          </ul>
+          <div className="divider lg:divider-horizontal"></div>
+          <article className="user-nav  mr-4">
+            <div className="avatar placeholder">
+              <div className="bg-neutral text-neutral-content w-12 rounded-full">
+                <span className="text-2xl font-bold">A</span>
+              </div>
             </div>
-          </li>
-         
-        </ul>
-        <div className="divider lg:divider-horizontal"></div>
-        <article className="user-nav  mr-4">
-          <div className="avatar placeholder">
-            <div className="bg-neutral text-neutral-content w-12 rounded-full">
-              <span className="text-2xl font-bold">A</span>
-            </div>
-          </div>
-          <h2 className="text-3xl font-bold ml-2"> Adeola</h2>
-          <IoIosArrowDown className="text-2xl cursor-pointer" />
-        </article>
+            <h2 className="text-3xl font-bold ml-2"> Adeola</h2>
+            <IoIosArrowDown className="text-2xl cursor-pointer" />
+          </article>
+        </div>
       </div>
-    </div>
+      <div style={{ height: 0 }} className="nav-tooltip">
+        <Tooltip visible>Noticification</Tooltip>
+      </div>
+      <div style={{ height: 0 }} className="nav-tooltip">
+        <Tooltip visible>Theme</Tooltip>
+      </div>
+    </>
   );
 };
 
