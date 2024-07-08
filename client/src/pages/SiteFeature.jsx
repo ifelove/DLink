1b2ed30345c7afdd87e904f5d80b2e9007fdf9e6
Ifeoluwa <ifel1939@gmail.com>
2024-07-08 21:37:43 +0100

    adding site features

--- client/src/pages/SiteFeature.jsx
+++ client/src/pages/SiteFeature.jsx
@@ -1,19 +1,40 @@
-
 import React from "react";
 
 const SiteFeature = () => {
+  const links = ["Overview", "Controller", "Components", "Alert", "Navigation"];
+  const [activeLink, setActiveLink] = React.useState("Controller");
+
+  const handleLink = (active) => {
+    setActiveLink(active);
+  };
+
   return (
     <section className="">
       <div className=" opacity-4 site-nav m-12 mb-7 rounded-xl ">
-        {" "}
-        <h2 className="site-nav-link text-xl font-bold cursor-pointer active">
+        {links.map((link, index) => {
+          const className = `site-nav-link text-xl font-bold cursor-pointer ${
+            link === activeLink ? "active" : ""
+          }`;
+
+          return (
+            <h2
+              className={className}
+              key={index}
+              onClick={() => handleLink(link)}
+            >
+              {link}
+            </h2>
+          );
+        })}
+
+        {/**  <h2 className="site-nav-link text-xl font-bold cursor-pointer active">
           Overview
         </h2>
         <h2 className="site-nav-link text-xl font-bold cursor-pointer">
           {" "}
           Controller
         </h2>
-        <h2 className="site-nav-link text-xl font-bold cursor-pointer active">
+        <h2 className="site-nav-link text-xl font-bold cursor-pointer ">
           {" "}
           Components
         </h2>
@@ -23,14 +44,18 @@ const SiteFeature = () => {
         </h2>
         <h2 className="site-nav-link text-xl font-bold cursor-pointer">
           Navigation to Site
-        </h2>
+        </h2>*/}
       </div>
 
       <div className="site-main">
-        <h2 className="site-main-header ml-10 text-2xl ">Site Overview</h2>
+        <h2 className="site-main-header ml-10 text-2xl ">IHS_LAG_0006A {activeLink}</h2>
         {/**/}
 
-        <article className=" form-article ">
+        <article
+          className={`${
+            activeLink === "Overview" ? "form-article active" : "form-article"
+          }`}
+        >
           <div className="site-div m-3">
             <label htmlFor="" className="text-2xl">
               Site ID
@@ -95,7 +120,11 @@ const SiteFeature = () => {
             </select>
           </div>
         </article>
-        <article className=" form-article">
+        <article
+          className={`${
+            activeLink === "Controller" ? "form-article active" : "form-article"
+          }`}
+        >
           <div className="site-div m-3">
             <label htmlFor="" className="text-2xl">
               Controller S/N
@@ -143,7 +172,11 @@ const SiteFeature = () => {
           </div>
         </article>
 
-        <article className=" form-article active">
+        <article
+          className={`${
+            activeLink === "Components" ? "form-article active" : "form-article"
+          }`}
+        >
           <div className="site-div m-3 ">
             <label htmlFor="" className="text-2xl w-96">
               Select Component
@@ -157,9 +190,9 @@ const SiteFeature = () => {
               <option>Camera</option>
             </select>
           </div>
-          <div className="divider"></div>
+         
 
-          <div className="card-list">
+          <div className="card-list ">
             <div className="card bg-base-100 w-96 shadow-xl ml-4 mt-2">
               <div className="card-body p-0">
                 <div className="pl-20">
@@ -264,7 +297,22 @@ const SiteFeature = () => {
 
         {/**<div type="checkbox" aria-label="Checkbox" className="btn">
           Update
+
+
+
         </div> */}
+
+        {activeLink === "Overview" && (
+          <div type="checkbox" aria-label="Checkbox" className="btn">
+            Update
+          </div>
+        )}
+
+        {activeLink === "Controller" && (
+          <div type="checkbox" aria-label="Checkbox" className="btn">
+            Replace Controller
+          </div>
+        )}
       </div>
     </section>
   );
