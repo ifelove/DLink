386dcd52df3d7a3cbd1c56742573d992ebc58371
Ifeoluwa <ifel1939@gmail.com>
2024-07-08 21:38:29 +0100

    adding site features

--- microservices/site/controller/SiteController.js
+++ microservices/site/controller/SiteController.js
@@ -10,28 +10,28 @@ const {
 
 //Noc
 
-//assign site to fse
-//check site is assigned to fse
-//check site details
-//update site details
-//get all sites
-//get all sites assigned to fse
-//track site address
+                  //assign site to fse
+                 //check site is assigned to fse
+///check site details
+///update site details
+                  //get all sites
+                  //get all sites assigned to fse
+///track site address
 
 //fse
 
-//get all sites assigned to fse
-//get site details
-//Replacement of component(installing and uninstalling component)
-//Replacement of controller
-//track site address
-//update site details
+                   //get all sites assigned to fse
+                  //get site details
+///Replacement of component(installing and uninstalling component)
+///Replacement of controller
+///track site address
+///update site details
 
 //Lab
 
-//get all sites assigned to fse
-//get site details
-//update site details
+                       //get all sites assigned to fse
+                      //get site details
+///update site details
 
 const createSite = async (req, res) => {
   const {
